c54036bc2aa1e6d7b35dae2edb6b61ef5086b7a8
Stepan Suvorov <stevermeister@gmail.com>
2014-11-23 17:56:26 +0100

    rebuilt logic of event-sorting

--- src/javascript/app/calendar/calendarService.js
+++ src/javascript/app/calendar/calendarService.js
@@ -1,34 +1,102 @@
-app.service('CalendarService', function() {
+angular.module('calendar').service('CalendarService', function($window) {
+  'use strict';
 
-  this.normalize = function(events) {
+  var _events = [],
+    _columns = [];
 
-    for(var i = 0; i < events.length; i++){
-      for(var j = i; j < events.length; j++){
-        if(i === j){
-          continue;
-        }
+  this.addEvent = function(event) {
 
-        if(isCollision(events[i], events[j])){
-          events[i].collisions = events[i].collisions || [];
-          events[i].collisions.push(j);
-          events[i].row = events[i].row || 0;
-          events[j].row = events[j].row || 0;
-          events[j].row = events[i] + 1;
-        }
-
-      }
-    }
-
-    function isCollision(event1, event2) {
-      if(event1.end < event2.end && event2.start < event1.end
-        || event2.end < event1.end && event1.start < event2.end){
-        return true;
-      }
-
+    if(!_checkEvent(event)){
+      $window.console.error('Event dimensions are not correct: (' + event.start + ', ' + event.end + ')' );
       return false;
     }
-    
-    return events;
+
+    var newEventIndex = _events.length,
+      columnCollisionEvents = [],
+      totalCollisionElements = [],
+      injected = false;
+    event.siblings = [];
+    event.column = 0;
+    _events.push(event);
+
+    for(var i = 0; i < _columns.length; i++){
+      columnCollisionEvents = _getColumnCollisionEvent(event, _columns[i]);
+      if(columnCollisionEvents.length === 0 && !injected){
+        event.column = i;
+        _columns[i].push(newEventIndex);
+        injected = true;
+      } else {
+        _addSiblingColumn(event, i);
+        totalCollisionElements = totalCollisionElements.concat(columnCollisionEvents);
+      }
+    }
+
+    if(!injected){
+      //no place - we need to create new column
+      event.column = _columns.length;
+      _columns[_columns.length] = [newEventIndex];
+    }
+
+    _updateSiblings(totalCollisionElements, event.column);
+
+    return event;
   };
-  
-  });
+
+  this.setEvents = function(newEvents) {
+    _events = [];
+    _columns = [];
+    for(var i = 0; i < newEvents.length; i++){
+      this.addEvent(newEvents[i]);
+    }
+    return _events;
+  };
+
+
+  function _isCollision(event1, event2) {
+
+    return (
+        event1.end >= event2.end && event1.start <= event2.start  ||
+        event1.end <= event2.end && event1.start >= event2.start  ||
+        event1.end <= event2.end && event1.end > event2.start ||
+        event1.start >= event2.start && event1.start < event2.end);
+  }
+
+  function _getColumnCollisionEvent(event, column){
+    var collisionEvents = [];
+    for(var i = 0; i < column.length; i++){
+      if(_isCollision(event, _events[column[i]])){
+        collisionEvents.push(column[i]);
+      }
+    }
+    return collisionEvents;
+  }
+
+  function _updateSiblings(eventSiblingIndexes, column){
+    for(var i = 0; i < eventSiblingIndexes.length; i++){
+      _addSiblingColumn(_events[eventSiblingIndexes[i]], column);
+    }
+  }
+
+  function _addSiblingColumn(event, column){
+    if(event.siblings.indexOf(column) === -1){
+      event.siblings.push(column);
+    }
+  }
+
+  function _checkEvent(event){
+
+
+    if(!angular.isNumber(event.start) ||
+       !angular.isNumber(event.end) ||
+       event.start < 0 ||
+       event.end < 0 ||
+       event.start > 720 ||
+       event.end > 720
+    ){
+      return false;
+    }
+
+    return true;
+  }
+
+});
